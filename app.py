7d9c3347ec7315a55389217e651861719f7f0b39
azocher <anna.zocher@gmail.com>
2020-03-26 14:35:26 -0700

    added pie default get route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app=Flask(__name__)
 
@@ -11,3 +11,8 @@ def hello_world():
 def greeting():
     return render_template('index.html', name="Akilah")
 
+
+@app.route('/pie')
+def pie():
+    ingredients = ["Flour", "Butter", "Apples"]
+    return jsonify({'pie ingredient': ingredients[0]})
