e7cfda034767a4be298f24add6dfe2fd59098265
tinnerrss <tinnerrss@hotmail.com>
2020-03-27 10:18:58 -0700

    added form and get/post route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template, jsonify
+from flask import Flask , render_template, jsonify, request
 
 app=Flask(__name__)
 
@@ -11,12 +11,30 @@ def greeting():
     owner = "Akilah"
     return render_template('index.html', owner=owner)
 
+ #global variable
+ ingredients = ['Apple', 'Eggs', 'Flour']   
+
 @app.route('/pie')
 def pie():
-    recipes = {
-        'pie ingredients': ['apples', 'sugar', 'flour']
-        }
-    return jsonify({'pie ingredients': recipes['pie ingredients'][0]})
+    # recipes = {
+    #     'pie ingredients': ['apples', 'sugar', 'flour']
+    #     }
+    # return jsonify({'pie ingredients': recipes['pie ingredients'][0]})
+
+    # ANNAS CODEALONG VVVV
+    if request.method == 'POST':
+        global ingredients
+
+        ingredients = request.form['ingredients']
+        ingredients.append(ingredient)
+        print(ingredients)
+        #shows random ingredient once user has inputted an new ingredient in form
+        index = random.randint(0, (len(ingredients)-1))
+
+        return jsonify({'pie ingredient': ingredients[index]})
+
+    else:
+        return render_template('pieForm.html')
 
 
 
