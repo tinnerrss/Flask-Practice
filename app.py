3071689fece4059287024db66b7392e6463667c2
tinnerrss <tinnerrss@hotmail.com>
2020-03-26 18:13:55 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask , render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
+
+@app.route('/greeting')
+def greeting():
+    owner = "Akilah"
+    return render_template('index.html', owner=owner)
+
+# @app.route('/pie')
+# def pie():
+
